9182aa0cd1dca6708fafa5008bafd659e6686f98
fendnugraha <fendnugraha92@gmail.com>
2025-01-20 11:02:47 +0700

    add xsrf

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': getCookie('XSRF-TOKEN'),
         Accept: 'application/json',
         'Content-Type': 'application/json',
     },
